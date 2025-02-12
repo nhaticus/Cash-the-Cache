714a0085ca2409f47e307b32867f6fb091424242
Epsilon-Galaxy <ajaycausewhynot@gmail.com>
2025-02-11 23:43:32 -0800

    created an upgrade manager and two basic upgrades
    created an upgrade speed and upgrade max weight. Moved some variables to singletons for upgrade Manager and Player Manager

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -6,6 +6,12 @@ public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager Instance;
     private PlayerCam playerCameraScript;
+    private PlayerMovement playerMovementScript;
+
+    //Player Stats
+    [SerializeField]
+    private int weight = 0;
+    private int maxWeight = 30;
 
     //Create a Singleton
     private void Awake()
@@ -19,8 +25,55 @@ public class PlayerManager : MonoBehaviour
             Destroy(gameObject);
         }
 
+        //Finds reference to playerCamera 
         playerCameraScript = GameObject.Find("Main Camera").GetComponent<PlayerCam>();
+        playerMovementScript = GameObject.Find("Player").GetComponent<PlayerMovement>();
     }
+
+    public void increaseMoveSpeed(float speedIncrease)
+    {
+        playerMovementScript.moveSpeed += speedIncrease;
+    }
+
+    public void decreaseMoveSpeed(float speedDecrease)
+    {
+        playerMovementScript.moveSpeed -= speedDecrease;
+    }
+
+    public float getMoveSpeed()
+    {
+        return playerMovementScript.moveSpeed;
+    }
+
+    public void addWeight(int itemWeight)
+    {
+        this.weight += itemWeight;
+    }
+
+    public void subWeight(int itemWeight)
+    {
+        this.weight -= itemWeight;
+    }
+
+    public int getWeight()
+    {
+        return this.weight;
+    }
+
+    public void increaseMaxWeight(int increase)
+    {
+        this.maxWeight += increase;
+    }
+    public void decreaseMaxWeight(int decrease)
+    {
+        this.maxWeight -= decrease;
+    }
+
+    public int getMaxWeight()
+    {
+        return this.maxWeight;
+    }
+
 
     //Locks rotation of player camera
     public void lockRotation()
