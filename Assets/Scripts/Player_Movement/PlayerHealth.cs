ff3ce2d51694aaac2c47c0ac3b8648e7436e54f5
Beepered <bktrieu@gmail.com>
2025-02-22 21:56:24 -0800

    van canvas

---
+++ Assets/Scripts/Player_Movement/PlayerHealth.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Script attached to a player hurtbox
+ * Defines the player health and what happens when they are hurt
+ */
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 1;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            OnDamage(1);
+        }
+    }
+
+    void OnDamage(int damage)
+    {
+        Debug.Log("damaged");
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Debug.Log("dead");
+            PlayerManager.Instance.setMoveSpeed(0);
+            PlayerManager.Instance.ableToInteract = false;
+        }
+    }
+}
