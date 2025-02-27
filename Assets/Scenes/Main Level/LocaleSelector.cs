16de52b4e7e18d48189868bee60de2d48de51044
Jacqueline <jacqueline.gracey@gmail.com>
2025-02-27 11:13:14 -0800

    Main Menu Localized
    Main menu localized via dropdown choice
    -Title (Cash the Cache) unchanged
    -Level not localized

---
+++ Assets/Scenes/Main Level/LocaleSelector.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Localization.Settings;
+
+
+public class LocaleSelector : MonoBehaviour
+{
+    bool active = false;
+    public void ChangeLocale(int localeID){
+        if(active) return;
+        StartCoroutine(SetLocale(localeID));
+
+    }
+
+
+    IEnumerator SetLocale(int _localeID){
+        active = true;
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID];
+        // English 0, French 1, Spanish 2
+        active = false;
+    }
+}
