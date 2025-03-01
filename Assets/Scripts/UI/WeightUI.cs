195be08be05948e3832d2fccc64fda8511f2e32f
Beepered <bktrieu@gmail.com>
2025-03-01 00:37:02 -0800

    weight UI bar

--- Assets/Scripts/UI/WeightUI.cs
+++ Assets/Scripts/UI/WeightUI.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using TMPro;
 using UnityEngine;
 
 public class WeightUI : MonoBehaviour
 {
     [SerializeField] TMP_Text weightText, maxWeightText;
+    [SerializeField] RectTransform fillBarTransform;
     PlayerInteract playerInteract;
 
     public void Initialize(GameObject _player)
@@ -19,5 +21,13 @@ public class WeightUI : MonoBehaviour
     public void UpdateWeightDisplay()
     {
         weightText.text = PlayerManager.Instance.getWeight().ToString();
+
+        float percentage = PlayerManager.Instance.getWeight() / (float) PlayerManager.Instance.getMaxWeight();
+        Debug.Log("get weight: " + PlayerManager.Instance.getWeight());
+        Debug.Log("MAX weight: " + PlayerManager.Instance.getMaxWeight());
+        Debug.Log(percentage + "%");
+        float newHeight = GetComponent<RectTransform>().sizeDelta.y * percentage;
+        Debug.Log("newHeight: " + newHeight);
+        fillBarTransform.sizeDelta = new Vector2(fillBarTransform.sizeDelta.x, newHeight);
     }
 }
