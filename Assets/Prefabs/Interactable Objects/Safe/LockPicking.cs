c0f45ecc6f378c3b53c49dc8e637fc2007bbf0e8
EPaz0 <enrique_pazg@yahoo.com>
2025-04-09 13:44:48 -0700

    Replaced old safes and added a new one in the laundry room

--- Assets/Prefabs/Interactable Objects/Safe/LockPicking.cs
+++ Assets/Prefabs/Interactable Objects/Safe/LockPicking.cs
@@ -251,7 +251,7 @@ public class LockPicking : MonoBehaviour, InteractEvent
     private IEnumerator ShowFailedMessage()
     {
         if (failedText) failedText.SetActive(true);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1.5f);
         if (failedText) failedText.SetActive(false);
         MarkLocked();
         ExitLockpicking();
