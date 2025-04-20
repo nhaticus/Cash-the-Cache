599ee25fb1afc5de49f48e2dc65fa5ee30980cad
Beepered <bktrieu@gmail.com>
2025-04-19 23:03:45 -0700

    added new npc models, drivable van in map scene

--- Assets/Scenes/Enrique test/Speedometer.cs
+++ Assets/Scenes/Enrique test/Speedometer.cs
@@ -7,33 +7,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
-
-
-
 public class Speedometer : MonoBehaviour
 {
     private const float MAX_SPEED_ANGLE = -90f;
     private const float ZERO_SPEED_ANGLE = 0;
 
     private Transform needTransform;
-    private Transform speedLabelTemplateTransform;
-
-
+    [SerializeField] GameObject speedLabelTemplate;
 
     private float speedMax;
     private float currentSpeed;
 
     [SerializeField] private CarController carController;
 
-    // Start is called before the first frame update
     private void Awake()
     {
         needTransform = transform.Find("Needle");
-        speedLabelTemplateTransform = transform.Find("SpeedLabelTemplate");
-        speedLabelTemplateTransform.gameObject.SetActive(false);
-
+        speedLabelTemplate.gameObject.SetActive(false);
 
         currentSpeed = 0f;
         speedMax = 100f;
@@ -41,7 +32,6 @@ public class Speedometer : MonoBehaviour
         CreateSpeedLabels();
     }
 
-    // Update is called once per frame
     void Update()
     {
         currentSpeed = carController.GetCurrentSpeedKMH();
@@ -55,13 +45,15 @@ public class Speedometer : MonoBehaviour
         int labelAmount = 5;
         float totalAngleSize = ZERO_SPEED_ANGLE - MAX_SPEED_ANGLE;
         for (int i = 0; i<= labelAmount; i++) {
-            Transform speedLabelTransform = Instantiate(speedLabelTemplateTransform, transform);
+            GameObject speedLabel = Instantiate(speedLabelTemplate, transform);
             float labelSpeedNormalized = (float)i / labelAmount;
             float speedLabelAngle = ZERO_SPEED_ANGLE + labelSpeedNormalized * totalAngleSize;
-            speedLabelTransform.eulerAngles = new Vector3(0, 0, speedLabelAngle);
-            speedLabelTransform.Find("SpeedText").GetComponent<TMP_Text>().text = Mathf.RoundToInt((1f-labelSpeedNormalized) * speedMax).ToString();
-            speedLabelTransform.Find("SpeedText").eulerAngles = Vector3.zero;
-            speedLabelTransform.gameObject.SetActive(true);
+            speedLabel.transform.eulerAngles = new Vector3(0, 0, speedLabelAngle);
+            Transform speedText = speedLabel.transform.Find("SpeedText");
+            speedText.GetComponent<TMP_Text>().text = Mathf.RoundToInt((1f-labelSpeedNormalized) * speedMax).ToString();
+            speedText.GetComponent<TMP_Text>().fontSize = 20;
+            speedText.eulerAngles = Vector3.zero;
+            speedLabel.SetActive(true);
         }
     }
 
@@ -70,9 +62,7 @@ public class Speedometer : MonoBehaviour
 
         float speedNormalized = currentSpeed / speedMax;
 
-
         return ZERO_SPEED_ANGLE - speedNormalized * totalAngleSize;
     }
-
 
 }
