813485c29ccda687e3d66b1f149d6ba031842888
Beepered <bktrieu@gmail.com>
2025-05-15 13:47:46 -0700

    shop colliders

--- Assets/Mapbox/IMPORTANT/BuildingInfo.cs
+++ Assets/Mapbox/IMPORTANT/BuildingInfo.cs
@@ -4,7 +4,7 @@ public class BuildingInfo : MonoBehaviour
 {
     public bool isShop = false;
     public int difficulty = 1; // range: 1-5
-    public int floors = 1; // range: 1-3
+    //public int floors = 1; // range: 1-3
 
     [SerializeField] bool difficultyFromSize = true;
 
@@ -24,12 +24,14 @@ public class BuildingInfo : MonoBehaviour
             difficulty = (int)Mathf.Floor((meshSize.x + meshSize.y) / 10 + Random.Range(-0.5f, 1.6f)); // difficulty is changed by a little
             if (difficulty < 1)
                 difficulty = 1;
+            /*
             if (meshSize.y < 3)
                 floors = 1;
             else if (meshSize.y < 6)
                 floors = 2;
             else
                 floors = 3;
+            */
         }
 
         transform.parent.tag = "Wall";
