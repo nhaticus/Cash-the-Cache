d6e0457882ecc0a4ab331b761846a5b09665328f
Beepered <bktrieu@gmail.com>
2025-05-22 14:09:25 -0700

    move collab stuff

--- Assets/Scripts/Map Scene/BasicBuildingCanvas.cs
+++ Assets/Scripts/Map Scene/BasicBuildingCanvas.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 /*
  * Class that inherits from BaseCanvasType
- * Just used to create a canvas that follows player camera and doesn't use any special variables
+ * Used to create a canvas that follows player camera and doesn't use any special variables
  */
 
-public class ShopCanvas : BaseCanvasType
+public class BasicBuildingCanvas : BaseCanvasType
 {
 }
